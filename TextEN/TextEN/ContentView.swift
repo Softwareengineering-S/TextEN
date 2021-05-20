193e2b68c28fd770873662288e6b47b4a53661cc
Softwareengineering-S <79382145+Softwareengineering-S@users.noreply.github.com>
2021-05-20 09:49:50 +0200

    added files via upload

---
+++ TextEN/TextEN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextEN
+//
+//  Created by Monique Shaqiri on 04.05.21.
+//  Copyright © 2021 Monique Shaqiri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
